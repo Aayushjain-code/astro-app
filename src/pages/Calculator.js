b7fe0532bca31eb6581b9d06a028f2698c55ee04
Aayush Jain <aayushjain6020@gmail.com>
2023-03-29 10:37:25 +0530

    Basic calculator setup

---
+++ src/pages/Calculator.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Calculator = () => {
+  return (
+    <View>
+      <Text>Calculator</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
+
+export default Calculator;
